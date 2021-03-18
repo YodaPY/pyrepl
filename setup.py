f32203eaba4049a8d563d4718c6c0a58517aec71
Yoda <YodaPY@github.com>
2021-03-18 16:53:47 +0100

    pip ready

---
+++ setup.py
@@ -0,0 +1,29 @@
+from pyrepl import __version__ as version
+from setuptools import setup, find_namespace_packages
+
+def long_description():
+    with open("README.md") as fp:
+        return fp.read()
+
+setup(
+    name="pyrepl",
+    version=version,
+    description="pyrepl allows for modification of the standard python REPL",
+    long_description=long_description(),
+    long_description_content_type="text/markdown",
+    author="YodaPY",
+    url="https://github.com/YodaPY/revents",
+    packages=find_namespace_packages(include=["pyrepl" + "*"], exclude=["pyrepl/test.pyr"]),
+    include_package_data=True,
+    python_requires=">=3.7.0,<3.10",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+)
